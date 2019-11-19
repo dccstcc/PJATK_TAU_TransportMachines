8ac42ab596a83326f7e49c594ec62e7981a85dc2
Dominik Stec <dominik@Dominik.Ubuntu>
2019-11-19 19:56:02 +0100

    TDD, add @BeforeClass test method

--- TransportMachinesApp/src/test/java/pl/edu/pjatk/tau/service/CarServiceTest.java
+++ TransportMachinesApp/src/test/java/pl/edu/pjatk/tau/service/CarServiceTest.java
@@ -5,12 +5,20 @@ import pl.edu.pjatk.tau.domain.Car;
 
 import java.util.TreeMap;
 
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 //import org.hamcrest.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CarServiceTest {
+	
+	public static CarService carService;
+	
+	@BeforeClass
+	public static void initCarServiceClass() {
+		carService = new CarService();
+	}
 	
 	@Test
 	public void ShouldInitializeEmptyDatabase() {
@@ -20,7 +28,7 @@ public class CarServiceTest {
 
 	@Test
 	public void ShouldReturnCarOfKnownId() {
-		CarService carService = new CarService();
+		//CarService carService = new CarService();
 		assertNotNull(carService.readById(1));
 	}
 
