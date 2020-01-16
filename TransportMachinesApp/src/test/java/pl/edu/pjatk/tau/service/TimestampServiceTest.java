3026c31d3c701dac2f6b65533e8b1c903dc5b1c0
dominiqxiqx <domste.domste@gmail.com>
2020-01-16 11:52:40 +0100

    Test environment initialization

---
+++ TransportMachinesApp/src/test/java/pl/edu/pjatk/tau/service/TimestampServiceTest.java
@@ -0,0 +1,16 @@
+package pl.edu.pjatk.tau.service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TimestampServiceTest {
+	
+	@Test
+	public void shouldPass() {
+		assertTrue(true);
+	}
+}
