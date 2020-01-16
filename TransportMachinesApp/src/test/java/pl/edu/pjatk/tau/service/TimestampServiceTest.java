c1bf7aad659af5e3b7f0e5ce7ad75ec8c85afab6
dominiqxiqx <domste.domste@gmail.com>
2020-01-16 19:12:38 +0100

    First initial tests

--- TransportMachinesApp/src/test/java/pl/edu/pjatk/tau/service/TimestampServiceTest.java
+++ TransportMachinesApp/src/test/java/pl/edu/pjatk/tau/service/TimestampServiceTest.java
@@ -1,16 +1,70 @@
 package pl.edu.pjatk.tau.service;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
+
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(JUnit4.class)
+import pl.edu.pjatk.tau.domain.CarTimestamp;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CarTimestamp.class, LocalDateTime.class})
 public class TimestampServiceTest {
+		
+	public CarTimestamp cars;
+
+	public LocalDateTime readTime;
+	public LocalDateTime retReadT;
+	public LocalDateTime writeTime;
+	public LocalDateTime retWriteT;
+	public LocalDateTime updateTime;
+	public LocalDateTime retUpdateT;
+	
+	//prepare Mock
+	@Before
+	public void setUp() {
+		cars = new CarTimestamp();
+		
+		PowerMockito.mockStatic(LocalDateTime.class);
+		
+		  readTime = PowerMockito.mock(LocalDateTime.class);
+		  retReadT = PowerMockito.mock(LocalDateTime.class);
+		  writeTime = PowerMockito.mock(LocalDateTime.class);
+		  retWriteT = PowerMockito.mock(LocalDateTime.class);
+		  updateTime = PowerMockito.mock(LocalDateTime.class);
+		  retUpdateT = PowerMockito.mock(LocalDateTime.class);
+		
+		PowerMockito.when(retReadT.getHour()).thenReturn(1);
+		PowerMockito.when(readTime.now()).thenReturn(retReadT);
+		
+		PowerMockito.when(retWriteT.getHour()).thenReturn(2);
+		PowerMockito.when(writeTime.now()).thenReturn(retWriteT);
+		
+		PowerMockito.when(retUpdateT.getHour()).thenReturn(3);
+		PowerMockito.when(updateTime.now()).thenReturn(retUpdateT);
+	}
+	
 	
 	@Test
 	public void shouldPass() {
 		assertTrue(true);
+	}
+	
+	@Test
+	public void shouldInitializeAllTimestampsDirectlyAddedIntoDatabase() {
+		cars.setReadTimestamp(readTime);
+		assertNotNull(cars.getReadTimestamp());
+		cars.setWriteTimestamp(readTime.now());
+		assertNotNull(cars.getWriteTimestamp());
+		cars.setUpdateTimestamp(LocalDateTime.now());
+		assertNotNull(cars.getUpdateTimestamp());
 	}
 }
